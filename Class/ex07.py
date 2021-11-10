03506a9bb2879a3f171004416dd688353473a6e2
Kmsz <kkamasooz@gmail.com>
2021-11-09 22:33:55 -0300

    Exercício07 classes

---
+++ Class/ex07.py
@@ -0,0 +1,29 @@
+
+
+class Tamaguchi:
+    def __init__(self, nome, fome, idade, saúde):
+        self.nome = nome
+        self.fome = fome
+        self.idade = idade
+        self.saúde = saúde
+        self.humor = (fome + saúde) / 2
+
+    def AlterarNome(self, novo_nome):
+        self.nome = novo_nome
+        print(f"O nome do Tamaguchi foi alterado para: {self.nome}")
+
+    def VerFome(self):
+        print(f"A fome de {self.nome} é de: {self.fome}")
+
+    def VerSaúde(self):
+        print(f"{self.nome} está com :{self.saúde}")
+
+    def VerFome(self):
+        print(f"A fome de {self.nome} é de: {self.fome}")
+
+    def VerFome(self):
+        print(f"A fome de {self.nome} é de: {self.fome}")
+
+    
+
+        
