e4406cd7ccca1feffe86125586b35a911c4de00f
Kmsz <kkamasooz@gmail.com>
2021-12-23 16:05:23 -0300

    Update ex035.py

--- Past-Codes/ex035.py
+++ Past-Codes/ex035.py
@@ -1,5 +1,8 @@
 r1 = float(input('Digite um valor:'))
 r2 = float(input('Digite outro valor:'))
 r3 = float(input('Digite mais valor:'))
+
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r2 + r1:
-    print('Os segmentos digitados podem formar um triangulo!')
+    print("O segmento pode formar um triângulo")
+else:
+  print("Os segmentos não podem formar um triângulo.")
