08ff718829e2262b650e684873d3bfc8231611bb
Kmsz <kkamasooz@gmail.com>
2022-01-13 20:35:11 -0300

    Update ex071.py

--- Past-Codes/ex071.py
+++ Past-Codes/ex071.py
@@ -1,6 +1,8 @@
-print('-' * 40)
+bars = '-' * 40
+
+print(bars)
 print('\033[1mBDB - Banco Internacional')
-print('-' * 40)
+print(bars)
 
 opcao = 0
 
@@ -14,7 +16,7 @@ while opcao != 3:
     opcao = int(input('Digite sua escolha:'))
     if opcao == 0:
         valorSaque = int(input('Valor do saque: R$ '))
-        print('-' * 40)
+        print(bars)
 
         for nota in [200, 100, 50, 20, 10, 1]:
             quantidade = valorSaque // nota
@@ -23,24 +25,24 @@ while opcao != 3:
             if quantidade > 0:
                 print(f'Foram retirados R${valorSaque} da sua conta.')
                 print(f'{quantidade} notas de R$ {nota}')
-                print('-' * 40)
+                print(bars)
 
     if opcao == 1:
-        print('-' * 40)
+        print(bars)
         valorTransf = int(input('Digite o valor que quer transferir: R$'))
         conta = str(input('Digite a conta bancária que deseja transferir:'))
         print(f'Foram transferidos {valorTransf} para a conta {conta}.')
-        print('-' * 40)
+        print(bars)
 
     if opcao == 2:
-        print('-' * 40)
+        print(bars)
         valorDeposit = int(input('Digite o valor do depósito: R$'))
         print(f'Foram depósitados R${valorDeposit} em sua conta.')
-        print('-' * 40)
+        print(bars)
 
 if opcao == 3:
-    print('-' * 40)
+    print(bars)
     print('BDB - Agradece. Volte sempre!')
-    print('-' * 40)
+    print(bars)
 
 
