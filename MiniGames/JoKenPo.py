510b9a5dd4b1ec680e18d448b57930ecb38e0c06
Kmsz <kkamasooz@gmail.com>
2022-03-25 14:51:04 -0300

    Update JoKenPo.py

--- MiniGames/JoKenPo.py
+++ MiniGames/JoKenPo.py
@@ -1,60 +1,43 @@
-#JoKenPo
 
 from time import sleep
 from random import randint
 
-itens = ('pedra','papel','tesoura')
-computador = randint(0,2)
-print('JoKenPo!')
-print('[ 0 ] Pedra')
-print('[ 1 ] Papel')
-print('[ 2 ] Tesoura')
-escolha = int(input('Escolha um:'))
+kws = ["\033[31mThe player loses!", "\033[32mThe player wins!", "\033[31minvalid choice!"]
+items = ('Rock', 'Paper', 'Scissors') # Rock = 0, Paper = 1, Scissors = 2
+bot = randint(0,2)
+bar = "-" * 40 
 
-print('Jo')
-sleep(1)
-print('Ken')
-sleep(1)
-print('Po!')
-sleep(1)
+print(f"JoKenPo!\n{bar}\n[ 0 ] Rock\n[ 1 ] Paper\n[ 2 ] Scissors")
+move = int(input('Choice one: '))
+print("Jo"), sleep(1), print("Ken"), sleep(1), print(f"Po!\n{bar}")
+print(f'The bot chose: {items[bot]} \nThe player chose: {items[move]}')
 
-print('O computador escolheu:{}'.format(itens[computador]))
-print('O jogador escolheu:{}'.format(itens[escolha]))
-if computador == 0: #computado jogou pedra!
-    if escolha == 0:
-        print('\033[34mO jogador e a máquina escolheram: Pedra! Empate.')
-
-    elif escolha == 1:
-        print('\033[32mO jogador venceu!')
-
-    elif escolha == 2:
-       print('\033[31mO jogador perdeu!')
-
+if bot == 0: #bot chose rock
+    if move == 0:
+        print('\033[34mThe player and the bot chose: Rock! Tie.')
+    elif move == 1:
+        print(kws[1])
+    elif move == 2:
+       print(kws[0])
     else:
-        print('\033[31mEscolha inválida!')
-
-if computador == 1: #computador jogou papel!
-    if escolha == 0:
-        print('\033[31mO jogador perdeu!')
-
-    elif escolha == 1:
-        print('\033[34mO jogador e a máquina escolheram: Papel! Empate.')
-
-    elif escolha == 2:
-       print('\033[31mO jogador perdeu!')
-
+        print(kws[2])
+        
+if bot == 1: #Bot chose paper
+    if move == 0:
+        print(kws[0])
+    elif move == 1:
+        print('\033[34mThe player and the bot chose: Paper! Tie.')
+    elif move == 2:
+       print(kws[1])
     else:
-        print('\033[31mEscolha inválida!')
+        print(kws[2])
 
-if computador == 2: #computdor jogou tesoura!
-    if escolha == 0:
-        print('\033[32mO jogador venceu!')
-
-    elif escolha == 1:
-        print('\033[31mO jogador perdeu!')
-
-    elif escolha == 2:
-        print('\033[34mO jogador e a máquina escolheram: Tesoura! Empate.')
-
+if bot == 2: #Bot chose scissors
+    if move == 0:
+        print(kws[1])
+    elif move == 1:
+        print(kws[0])
+    elif move == 2:
+        print('\033[34mThe player and the bot chose: Scissors! Tie.')
     else:
-        print('\033[31mEscolha inválida!')
+        print(kws[2])
