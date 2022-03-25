dfccf20b59f0d587178ff80c117f805b1a3d227d
Kmsz <kkamasooz@gmail.com>
2022-03-25 15:27:51 -0300

    Create bk_sv2.py

---
+++ bk_sv2.py
@@ -0,0 +1,14 @@
+option = 0
+bar = "-" * 41 
+print(f"{bar}\n-\t\tBank Server\t\t- \n{bar}")
+
+while option != 3:
+    print("""- Select what you want to do -
+    [ 0 ] Withdraw
+    [ 1 ] Transfer
+    [ 2 ] Deposit
+    [ 3 ] """)
+    
+
+
+
