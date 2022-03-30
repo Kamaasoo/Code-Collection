e4b611b14d9aa87add74c74bbc6dc11b7cfb56b6
Kmszw_ <kkamasooz@gmail.com>
2022-03-29 23:46:43 -0300

    Update the-guesser.py

--- MiniGames/the-guesser.py
+++ MiniGames/the-guesser.py
@@ -1,4 +1,3 @@
-#The Guesser
 
 import os
 from random import randint
@@ -6,26 +5,22 @@ from random import randint
 num = randint(0, 50)
 tentativa = 0
 chute = 0
-print('\033[1m-' * 15, 'O adivinhador', '-' * 15)
+bar = "-" * 40
 
+print('\033[1m-' * 15, 'O adivinhador', '-' * 15)
 print('Foi sorteado um número de 0 a 50! \nTente adivinhar qual número foi gerado.')
+
 while chute != num:
 	chute = int(input('Qual o seu chute?: '))
 	if chute < num:
 		os.system('cls')
-		print('-' * 40)
-		print(f'O valor sorteado é maior que {chute}!')
+		print(f"{bar}\nOvalor sorteado é maior que {chute}!')
 		tentativa += 1
-
 	if chute > num:
 		os.system('cls')
-		print('-' * 40)
-		print(f'O valor sorteado é menor que {chute}!')
+		print(f'{bar}\nO valor sorteado é menor que {chute}!')
 		tentativa += 1
-
 	if chute == num:
 		os.system('cls')
-		print('-' * 40)
-		print(f'Você acertou! O número sorteado foi {num}!')
-		print(f'O seu número de tentativas foi de {tentativa}!')
-		print('-' * 40)
+		print(f'{bar}\nVocê acertou! O número sorteado foi {num}!')
+		print(f'O seu número de tentativas foi de {tentativa}!\n{bar}')
