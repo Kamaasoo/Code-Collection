1bc0a6f90eddded3587f97a198222cefbe9de47a
Kamaasoo_ <kkamasooz@gmail.com>
2022-11-02 16:43:02 -0300

    Update ex4.py

--- PythonOrg/Sequential Structure/ex4.py
+++ PythonOrg/Sequential Structure/ex4.py
@@ -1,7 +1,5 @@
-g1 = float(input("Insert a first grade: "))
-g2 = float(input("Insert a second grade: "))
-g3 = float(input("Insert a third grade: "))
-g4 = float(input("Insert a fourthy grade: "))
+g1 = float(input("Insert a first grade: "));g2 = float(input("Insert a second grade: "))
+g3 = float(input("Insert a third grade: "));g4 = float(input("Insert a fourthy grade: "))
 
 print(f"Your average is: {(g1 + g2 + g3 + g4) / 2}")
            
