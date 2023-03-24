ebfd93d5364df6d596e42e2236be8ebe925ab68d
Kamaasoo_ <kkamasooz@gmail.com>
2023-03-23 23:01:20 -0300

    bank-system.py
    Optimized code.

--- bank_system.py
+++ bank_system.py
@@ -3,18 +3,18 @@ print('\033[1mBDB - International bank')
 print(bar)
 
 while option != 3:
-    print('Escolha uma opção:\n[ 0 ] Saque\n [ 1 ] Transferência\n[ 2 ] Depósito\n[ 3 ] Sair')
-    option = int(input('Digite sua escolha:'))
+    print('Choose an option:\n[ 0 ] Withdrawal\n [ 1 ] Transfer\n[ 2 ] Deposit\n[ 3 ] Exit')
+    option = int(input('Your choise: '))
     
     if option == 0:
-        withdramt = int(input('Valor do saque: R$ '))
+        withdramt = int(input('Withdrawal value: R$ '))
         print(bar)
 
         for cell in [200, 100, 50, 20, 10, 1]:
             amount = withdramt // cell
             withdramt = withdramt % cell
 
-            if quantidade > 0:
+            if amount > 0:
                 print(f'R${withdramt} Have been removed from your account.')
                 print(f'{amount} cells R$ {cell}\n{bar}')
 
@@ -27,7 +27,7 @@ while option != 3:
     if option == 2:
         print(bar)
         depamount = int(input('Deposit value: '))
-        print(f'Foram depósitados R${depamount} em sua conta.\n{bar}')
+        print(f'R${depamount} were deposited in your account.\n{bar}')
 
 if option == 3:
-    print(f'{bar}\nBDB - Agradece. Volte sempre!\n{bar}')
+    print(f'{bar}\nBDB - Thank you. Check back often!\n{bar}')
