3c3044f354b51417e13f857b2cf79cdc9bd5f705
Kamaasoo_ <kkamasooz@gmail.com>
2023-03-23 22:55:42 -0300

    Update bank_system.py

--- bank_system.py
+++ bank_system.py
@@ -1,44 +1,33 @@
-print('-' * 40)
-print('\033[1mBDB - Banco Internacional')
-print('-' * 40)
+bar = '-' * 40
+print('\033[1mBDB - International bank')
+print(bar)
 
-opcao = 0
+while option != 3:
+    print('Escolha uma opção:\n[ 0 ] Saque\n [ 1 ] Transferência\n[ 2 ] Depósito\n[ 3 ] Sair')
+    option = int(input('Digite sua escolha:'))
+    
+    if option == 0:
+        withdramt = int(input('Valor do saque: R$ '))
+        print(bar)
 
-while opcao != 3:
-    print('Escolha uma opção:')
-    print('[ 0 ] Saque ')
-    print('[ 1 ] Transferência')
-    print('[ 2 ] Depósito ')
-    print('[ 3 ] Sair')
-
-    opcao = int(input('Digite sua escolha:'))
-    if opcao == 0:
-        valorSaque = int(input('Valor do saque: R$ '))
-        print('-' * 40)
-
-        for nota in [200, 100, 50, 20, 10, 1]:
-            quantidade = valorSaque // nota
-            valorSaque = valorSaque % nota
+        for cell in [200, 100, 50, 20, 10, 1]:
+            amount = withdramt // cell
+            withdramt = withdramt % cell
 
             if quantidade > 0:
-                print(f'Foram retirados R${valorSaque} da sua conta.')
-                print(f'{quantidade} notas de R$ {nota}')
-                print('-' * 40)
+                print(f'R${withdramt} Have been removed from your account.')
+                print(f'{amount} cells R$ {cell}\n{bar}')
 
-    if opcao == 1:
-        print('-' * 40)
-        valorTransf = int(input('Digite o valor que quer transferir: R$'))
-        conta = str(input('Digite a conta bancária que deseja transferir:'))
-        print(f'Foram transferidos {valorTransf} para a conta {conta}.')
-        print('-' * 40)
+    if option == 1:
+        print(bar)
+        tranfsamount = int(input('Transfer amount: R$'))
+        account = input('inform the account:'))
+        print(f'{tranfsamount} Were transferred to: {account}.\n{bar}')
 
-    if opcao == 2:
-        print('-' * 40)
-        valorDeposit = int(input('Digite o valor do depósito: R$'))
-        print(f'Foram depósitados R${valorDeposit} em sua conta.')
-        print('-' * 40)
+    if option == 2:
+        print(bar)
+        depamount = int(input('Deposit value: ))
+        print(f'Foram depósitados R${depamount} em sua conta.\n{bar}')
 
-if opcao == 3:
-    print('-' * 40)
-    print('BDB - Agradece. Volte sempre!')
-    print('-' * 40)
+if option == 3:
+    print(f'{bar}\nBDB - Agradece. Volte sempre!\n{bar}')
